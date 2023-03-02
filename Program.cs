347ca92dd9f00ea9f4294be13fed3cb0b582b9cb
Vaibhav <vaibhavovhal15@gmail.com>
2023-03-02 19:57:47 +0530

    [Vaibhav] Add:Hello

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Addition_of_two_numbers
+{
+    class Program //entry point of application
+    {
+        static void Main(string[] args)
+        {
+            int x = 15;
+            int y = 10;
+            int sum = x + y;
+            Console.WriteLine(sum);
+            Console.WriteLine("Hello World!")
+        }
+    }
+}
