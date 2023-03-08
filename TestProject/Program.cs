688d9dc4a0de2afc46101a1ae6002f27b43ecc63
Vaibhav <vaibhavovhal15@gmail.com>
2023-03-08 14:57:09 +0530

    [Vaibhav] Add:New_Addition

---
+++ TestProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Testproject;
+
+    public class Addition
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Enter the first Number: ");
+        int first_num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the second Number: ");
+        int second_num = Convert.ToInt32(Console.ReadLine());
+        int final_number = first_num + second_num;
+        Console.WriteLine("The Addition of two numbers:- " + final_number);
+        }
+    }
+
